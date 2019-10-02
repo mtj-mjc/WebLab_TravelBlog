4f6036dd7fb2486593006061dbcbf7777a9a3ef4
Matej Mrnjec <matej.mrnjec@hotmail.com>
2019-10-02 10:56:35 +0200

    Deleted unnecassary Tabs

--- modules/core/client/config/core-admin.client.menus.js
+++ modules/core/client/config/core-admin.client.menus.js
@@ -8,11 +8,12 @@
   menuConfig.$inject = ['menuService'];
 
   function menuConfig(menuService) {
-    menuService.addMenuItem('topbar', {
+ /*   menuService.addMenuItem('topbar', {
       title: 'Admin',
       state: 'admin',
       type: 'dropdown',
       roles: ['admin']
     });
+    */
   }
 }());
--- modules/posts/client/config/posts.client.menus.js
+++ modules/posts/client/config/posts.client.menus.js
@@ -8,11 +8,11 @@
   menuConfig.$inject = ['menuService'];
 
   function menuConfig(menuService) {
-    menuService.addMenuItem('topbar', {
+  /*  menuService.addMenuItem('topbar', {
       title: 'Posts',
       state: 'posts.list',
       roles: ['*']
     });
-
+*/
   }
 }());
